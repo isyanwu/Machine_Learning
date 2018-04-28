1bfed81111eba7d57cd26244aecd71532f54e00e
yanwu9887 <13777866443@163.com>
2018-04-27 23:01:38 -0700

    linear_regression

--- BasicModelsAndTensorflowCode/linear_regression.py
+++ BasicModelsAndTensorflowCode/linear_regression.py
@@ -1,1 +1,52 @@
-aaaa
+import tensorflow as tf
+import numpy as np
+import matplotlib.pyplot as plt
+
+#
+number_of_sample=50
+temp=np.arange(number_of_sample).reshape(1,number_of_sample)
+train_X_origin=temp+np.random.random((1,number_of_sample))
+train_Y_origin=3*temp+6+np.random.random((1,number_of_sample))
+
+temp=np.vstack([train_X_origin,train_Y_origin])
+mu=np.mean(temp)
+sigma=np.std(temp)
+#train_X=(train_X_origin-mu)/sigma
+#train_Y=(train_Y_origin-mu)/sigma
+maxx=np.max(temp)
+minn=np.min(temp)
+alp=maxx-minn
+train_X=(train_X_origin-minn)/(maxx-minn)
+train_Y=(train_Y_origin-minn)/(maxx-minn)
+
+
+#parameter
+descent_rate=0.1
+max_iteration=2000
+
+X=tf.placeholder(tf.float32)
+Y=tf.placeholder(tf.float32)
+
+w=tf.Variable(tf.random_uniform([1,1],2,3))
+b=tf.Variable(tf.zeros([1,1]))
+
+prediction=tf.add(tf.matmul(w,X),b)
+loss=tf.reduce_mean(tf.square(Y-prediction))/2
+
+optimizer=tf.train.GradientDescentOptimizer(descent_rate).minimize(loss)
+
+init=tf.global_variables_initializer()
+with tf.Session() as sess:
+    sess.run(init)
+    for i in range(max_iteration):
+        sess.run(optimizer,feed_dict={X:train_X,Y:train_Y})
+        if i%50==0:
+            temp=sess.run(loss,feed_dict={X:train_X,Y:train_Y})
+            print('iteration:','%04d'%i,'loss:','{:.9f}'.format(temp), \
+                  'w=',sess.run(w),'b=',alp*sess.run(b)+minn+minn*(1-sess.run(w)))
+    plt.plot(train_X_origin,train_Y_origin,'ro')
+    #plt.plot(train_X, train_Y, 'ro')
+    #plt.plot(train_X_origin.T,sess.run(w)*train_X_origin.T+sess.run(b))
+    plt.plot(train_X_origin.T, sess.run(w) * train_X_origin.T + alp*sess.run(b)+minn+minn*(1-sess.run(w)))
+    plt.show()
+
