7e7fa23c2cc75d5658ad185adb69bd56d4db1072
燕武 <38755286+yanwu9887@users.noreply.github.com>
2018-04-28 14:09:24 +0800

    更新注释

--- BasicModelsAndTensorflowCode/linear_regression.py
+++ BasicModelsAndTensorflowCode/linear_regression.py
@@ -2,15 +2,16 @@ import tensorflow as tf
 import numpy as np
 import matplotlib.pyplot as plt
 
-#
+#生成训练数据
 number_of_sample=50
 temp=np.arange(number_of_sample).reshape(1,number_of_sample)
 train_X_origin=temp+np.random.random((1,number_of_sample))
 train_Y_origin=3*temp+6+np.random.random((1,number_of_sample))
 
+#归一化或者标准化
 temp=np.vstack([train_X_origin,train_Y_origin])
-mu=np.mean(temp)
-sigma=np.std(temp)
+#mu=np.mean(temp)
+#sigma=np.std(temp)
 #train_X=(train_X_origin-mu)/sigma
 #train_Y=(train_Y_origin-mu)/sigma
 maxx=np.max(temp)
@@ -20,10 +21,11 @@ train_X=(train_X_origin-minn)/(maxx-minn)
 train_Y=(train_Y_origin-minn)/(maxx-minn)
 
 
-#parameter
+#梯度下降参数
 descent_rate=0.1
 max_iteration=2000
 
+#TensorFlow
 X=tf.placeholder(tf.float32)
 Y=tf.placeholder(tf.float32)
 
@@ -34,8 +36,8 @@ prediction=tf.add(tf.matmul(w,X),b)
 loss=tf.reduce_mean(tf.square(Y-prediction))/2
 
 optimizer=tf.train.GradientDescentOptimizer(descent_rate).minimize(loss)
-
 init=tf.global_variables_initializer()
+
 with tf.Session() as sess:
     sess.run(init)
     for i in range(max_iteration):
@@ -43,7 +45,7 @@ with tf.Session() as sess:
         if i%50==0:
             temp=sess.run(loss,feed_dict={X:train_X,Y:train_Y})
             print('iteration:','%04d'%i,'loss:','{:.9f}'.format(temp), \
-                  'w=',sess.run(w),'b=',alp*sess.run(b)+minn+minn*(1-sess.run(w)))
+                  'w=',sess.run(w),'b=',alp*sess.run(b)+minn+minn*(1-sess.run(w)))#反归一化
     plt.plot(train_X_origin,train_Y_origin,'ro')
     #plt.plot(train_X, train_Y, 'ro')
     #plt.plot(train_X_origin.T,sess.run(w)*train_X_origin.T+sess.run(b))
