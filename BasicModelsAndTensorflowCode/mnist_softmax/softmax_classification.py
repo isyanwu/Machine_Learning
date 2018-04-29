342e49c6d56002dcb4f4440e9a9ef097000217f6
燕武 <38755286+yanwu9887@users.noreply.github.com>
2018-04-29 16:10:22 +0800

    Update softmax_classification.py

--- BasicModelsAndTensorflowCode/mnist_softmax/softmax_classification.py
+++ BasicModelsAndTensorflowCode/mnist_softmax/softmax_classification.py
@@ -19,9 +19,9 @@ optimizer=tf.train.GradientDescentOptimizer(descent_rate).minimize(cross_entropy
 init=tf.global_variables_initializer()
 
 #评估模型
-##tf.argmax(input,axis),axis=1返回input每行的最大值所对应的索引值，axis则是列
+#tf.argmax(input,axis),axis=1返回input每行的最大值所对应的索引值，axis则是列
 correct_prediction = tf.equal(tf.argmax(Y, axis=1), tf.argmax(Y_prediction, axis=1))
-#tf.cast(input, dtype) 强制类型转化，
+#tf.cast(input, dtype) 强制类型转换
 accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 
 #train
