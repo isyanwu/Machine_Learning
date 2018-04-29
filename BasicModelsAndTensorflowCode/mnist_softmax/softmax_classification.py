83ece4a045fb6ec3c7b27f7b392aea38c6a7532f
燕武 <38755286+yanwu9887@users.noreply.github.com>
2018-04-29 15:35:37 +0800

    Update softmax_classification.py
    更新注释及部分代码

--- BasicModelsAndTensorflowCode/mnist_softmax/softmax_classification.py
+++ BasicModelsAndTensorflowCode/mnist_softmax/softmax_classification.py
@@ -2,35 +2,31 @@ import input_data
 import tensorflow as tf
 mnist=input_data.read_data_sets('MNIST_data/',one_hot=True)
 
-train_X=mnist.train.images
-train_Y=mnist.train.labels
-#print(train_X.shape)
-#print(train_Y.shape)
-batch_x,batch_y=mnist.train.next_batch(100)
-print(batch_x.shape)
-
+#梯度下降参数
 descent_rate=0.01
 max_iteration=1000
 
-X=tf.placeholder(tf.float32)
-Y=tf.placeholder(tf.float32)
+#TensorFlow流程图
+X=tf.placeholder(tf.float32,[None,784])
+Y=tf.placeholder(tf.float32,[None,10])
 
 W=tf.Variable(tf.zeros([784,10]))
 b=tf.Variable(tf.zeros([1,10]))
 
 Y_prediction=tf.nn.softmax(tf.matmul(X,W)+b)
-
 cross_entropy=-tf.reduce_sum(Y*tf.log(Y_prediction))
 optimizer=tf.train.GradientDescentOptimizer(descent_rate).minimize(cross_entropy)
 init=tf.global_variables_initializer()
 
+#评估模型
 correct_prediction = tf.equal(tf.argmax(Y, 1), tf.argmax(Y_prediction, 1))
 accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 
+#train
 with tf.Session() as sess:
     sess.run(init)
     for i in range(max_iteration):
         batch_x,batch_y=mnist.train.next_batch(100)
         sess.run(optimizer,feed_dict={X:batch_x,Y:batch_y})
 
-    print(sess.run(accuracy,feed_dict={X:train_X,Y:train_Y}))
+    print(sess.run(accuracy,feed_dict={X:mnist.test.images,Y:mnist.test.labels}))
